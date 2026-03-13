9211592a8d07ce734c9e95a79118d1c2533ddd8b
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-03-13 23:44:05 +0100

    fix dotfiles include

--- home/leif/default.nix
+++ home/leif/default.nix
@@ -1,24 +1,23 @@
-{ lib, config, username, ... }:
-let
-  dotfilesPath = "/etc/nixos/home/${username}/dotfiles";
-  link = name: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/${name}";
-in
+{ username, ... }:
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-
-  home-manager.users.${username} = {
-    imports = lib.filesystem.listFilesRecursive ./apps;
-
-    home.username = username;
-    home.homeDirectory = "/home/${username}";
-    home.stateVersion = "25.11";
-
-    home.file = builtins.listToAttrs (
-      map (name: {
-        name = ".config/${name}";
-        value.source = link name;
-      }) (builtins.attrNames (builtins.readDir ./dotfiles))
-    );
-  };
+  home-manager.users.${username} =
+    { config, lib, ... }:
+    let
+      dotfilesPath = "/etc/nixos/home/${username}/dotfiles";
+      link = name: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/${name}";
+    in
+    {
+      imports = lib.filesystem.listFilesRecursive ./apps;
+      home.username = username;
+      home.homeDirectory = "/home/${username}";
+      home.stateVersion = "25.11";
+      home.file = builtins.listToAttrs (
+        map (name: {
+          name = ".config/${name}";
+          value.source = link name;
+        }) (builtins.attrNames (builtins.readDir ./dotfiles))
+      );
+    };
 }
