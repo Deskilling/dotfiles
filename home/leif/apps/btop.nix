a9bc44db0b364e4eb3f671e14809e545ef14dda3
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-04-10 18:14:05 +0200

    move btop into nix file

---
+++ home/leif/apps/btop.nix
@@ -0,0 +1,16 @@
+{
+  programs.btop = {
+    enable = true;
+
+    settings = {
+      color_theme = "tokyo-night";
+      theme_background = false;
+
+      mem_below_net = true;
+
+      proc_left = true;
+      proc_sorting = "memory";
+      proc_filter_kernel = true;
+    };
+  };
+}
