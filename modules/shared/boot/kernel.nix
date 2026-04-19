85445df2579cdfb58ea254ebfa34bd8d1a0f6261
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-04-19 21:29:34 +0200

    more kernel params

--- modules/shared/boot/kernel.nix
+++ modules/shared/boot/kernel.nix
@@ -6,5 +6,10 @@
   boot.kernelParams = [
     "nowatchdog"
     "nmi_watchdog=0"
+    "tsc=nowatchdog"
+
+    "amdgpu.ppfeaturemask=0xffffffff"
+    "amdgpu.gpu_recovery=1"
+    "amdgpu.lockup_timeout=1000"
   ];
 }
