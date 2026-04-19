6ce91478b240d8b7f3a9251d3d80cc446df8e016
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-04-19 21:29:54 +0200

    keyd to change caps lock to hyperkey

--- hosts/desktop/modules/services.nix
+++ hosts/desktop/modules/services.nix
@@ -6,6 +6,7 @@
     ../../../modules/shared/services/cleanup.nix
     ../../../modules/shared/services/displaymanager.nix
     ../../../modules/shared/services/flatpak.nix
+    ../../../modules/shared/services/keyd.nix
     ../../../modules/shared/services/networking.nix
     ../../../modules/shared/services/update.nix
   ];
---
+++ modules/shared/services/keyd.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  services.keyd = {
+    enable = true;
+
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            capslock = "overload(hyper, esc)";
+          };
+
+          "hyper:C-M-S-A" = { };
+        };
+      };
+    };
+  };
+}
