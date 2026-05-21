330504735d3d408d05d62b96f91ca0a8dbe23941
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-05-21 21:43:02 +0200

    add resolved && force usage of cloudflare dns

--- modules/shared/services/networking.nix
+++ modules/shared/services/networking.nix
@@ -1,5 +1,40 @@
 { ... }:
 {
   networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+
+    ensureProfiles.profiles = {
+      "Wired connection 1" = {
+        connection = {
+          id = "Wired connection 1";
+          type = "ethernet";
+        };
+        ipv4 = {
+          method = "auto";
+          ignore-auto-dns = "true";
+        };
+        ipv6 = {
+          method = "auto";
+          ignore-auto-dns = "true";
+        };
+      };
+    };
+  };
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      DNSSEC = "false";
+      DNSOverTLS = "opportunistic";
+      FallbackDNS = [
+        "1.1.1.1"
+        "1.0.0.1"
+
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+      ];
+    };
+  };
 }
