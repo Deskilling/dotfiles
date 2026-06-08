a7f9aa6ad1ea0f6f4219e9ca4fcff376f2b0c476
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-06-08 22:50:10 +0200

    update zed.nix

--- home/leif/config/gui/zed.nix
+++ home/leif/config/gui/zed.nix
@@ -3,32 +3,101 @@
   programs.zed-editor = {
     enable = true;
     mutableUserSettings = false;
+    mutableUserKeymaps = false;
+
+    extensions = [
+      "nix"
+      "html"
+      "toml"
+      "git_firefly"
+      "make"
+      "lua"
+      "log"
+      "go_snippets"
+
+      "cattpuccin"
+    ];
 
     userSettings = {
-      theme = "Noctalia Dark";
+      # I will add Noctalia back once the template is available again in v5
+      theme = "Catppuccin Mocha";
       icon_theme = "Catppuccin Mocha";
-      title_bar = {
-        show_sign_in = false;
-        show_branch_icon = true;
+
+      active_pane_modifiers = {
+        border_size = 0.5;
+        inactive_opacity = 0.8;
       };
 
-      status_bar = {
-        cursor_position_button = false;
-      };
+      bottom_dock_layout = "full";
 
-      project_panel = {
-        dock = "right";
-      };
+      auto_signature_help = true;
+
+      base_keymap = "SublimeText";
+
+      diagnostics_max_severity = "hint";
 
       disable_ai = true;
-      diff_view_style = "unified";
+
+      load_direnv = "shell_hook";
+
       double_click_in_multibuffer = "open";
-      autosave = "on_focus_change";
-      auto_update = false;
+
+      gutter = {
+        runnables = false;
+      };
+
+      minimap = {
+        show = "auto";
+      };
+
+      tabs = {
+        git_status = true;
+        activate_on_close = "left_neighbour";
+        show_diagnostics = "all";
+      };
+
+      diagnostics = {
+        inline = {
+          enabled = true;
+        };
+      };
+
+      indent_guides = {
+        enabled = true;
+        coloring = "indent_aware";
+      };
+
+      journal = {
+        hour_format = "hour24";
+      };
+
+      languages = {
+        C = {
+          format_on_save = "on";
+        };
+
+        Lua = {
+          enable_language_server = false;
+        };
+      };
 
       telemetry = {
         diagnostics = false;
         metrics = false;
+      };
+
+      terminal = {
+        button = false;
+      };
+
+      title_bar = {
+        show_branch_icon = true;
+        show_user_picture = false;
+        show_sign_in = false;
+      };
+
+      project_panel = {
+        dock = "right";
       };
     };
   };
