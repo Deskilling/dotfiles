3791560bde93a617a6182fcc3b9e1a3a68891de9
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-06-18 22:07:08 +0200

    change from librewolf -> firefox

--- home/leif/desktop.nix
+++ home/leif/desktop.nix
@@ -13,12 +13,12 @@
     ./config/cli/nix.nix
 
     # gui
-    ./config/gui/essential.nix
     ./config/gui/cider.nix
+    ./config/gui/essential.nix
+    ./config/gui/firefox.nix
     ./config/gui/games.nix
     ./config/gui/jetbrains.nix
     ./config/gui/kitty.nix
-    ./config/gui/librewolf.nix
     ./config/gui/obs.nix
     ./config/gui/pear.nix
     ./config/gui/prismlauncher.nix
@@ -49,5 +49,5 @@
   home.username = "leif";
   home.homeDirectory = "/home/leif";
 
-  home.stateVersion = "25.11";
+  home.stateVersion = "26.05";
 }
