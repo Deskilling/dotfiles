86353b442621b9b22e7a881f6bcb33fdabe0b202
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-06-26 10:18:41 +0200

    update direnv

--- home/leif/config/shell/direnv.nix
+++ home/leif/config/shell/direnv.nix
@@ -4,5 +4,10 @@
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+
+    config.global = {
+      # Make direnv messages less verbose
+      hide_env_diff = true;
+    };
   };
 }
