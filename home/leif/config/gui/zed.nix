74fe383f86d337bb42d0d436b6db9f47cc3eb05d
Leif Haberecht <deskilling@tuta.com>
2026-09-02 01:22:56 +0200

    move zed into symlink

--- home/leif/config/gui/zed.nix
+++ home/leif/config/gui/zed.nix
@@ -10,8 +10,6 @@
 
   programs.zed-editor = {
     enable = true;
-    mutableUserSettings = false;
-    mutableUserKeymaps = false;
 
     extensions = [
       "nix"
@@ -29,82 +27,7 @@
 
     userSettings = {
       theme = if config.matugen.enable then "Matugen Dark Transparent" else "Catppuccin Mocha";
-      icon_theme = "Catppuccin Mocha";
-
-      active_pane_modifiers = {
-        border_size = 0.5;
-        inactive_opacity = 0.8;
-      };
-
-      bottom_dock_layout = "full";
-
-      auto_signature_help = true;
-
-      base_keymap = "SublimeText";
-
-      diagnostics_max_severity = "hint";
-
-      disable_ai = true;
-
-      load_direnv = "shell_hook";
-
-      double_click_in_multibuffer = "open";
-
-      gutter = {
-        runnables = false;
-      };
-
-      minimap = {
-        show = "auto";
-      };
-
-      tabs = {
-        git_status = true;
-        activate_on_close = "left_neighbour";
-        show_diagnostics = "all";
-      };
-
-      diagnostics = {
-        inline = {
-          enabled = true;
-        };
-      };
-
-      journal = {
-        hour_format = "hour24";
-      };
-
-      file_types = {
-        C = [
-          "c"
-          "h"
-        ];
-      };
-
-      languages = {
-        C = {
-          format_on_save = "on";
-        };
-
-      };
-
-      telemetry = {
-        diagnostics = false;
-        metrics = false;
-      };
-
-      terminal = {
-        button = false;
-      };
-
-      title_bar = {
-        show_user_picture = false;
-        show_sign_in = false;
-      };
-
-      project_panel = {
-        dock = "right";
-      };
     };
   };
-}
+
+  home.file.".config/zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/leif/config/gui/zed/settings.json";}
